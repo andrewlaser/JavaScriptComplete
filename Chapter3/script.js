d5307dfa64a41218f3c6673fbb3e7ac2953285dc
Andriy Lazurkevych <alazurkevych@n-ix.com>
2019-01-04 17:43:06 +0200

    Chapter3

---
+++ Chapter3/script.js
@@ -0,0 +1,84 @@
+///////////////////////////////////////
+// Lecture: Hoisting
+calculateAge(1969);
+function calculateAge(year)
+{
+    console.log(2019 - year);
+}
+
+
+
+var retirement = function(year)
+{
+    console.log(65 - (2016-year));
+}
+
+retirement(10);
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: The this keyword
+
+
+
+
+
+
+
+
+
